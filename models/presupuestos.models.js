5f3ada9e54a1099359ca22ce08a5fe48a3182ab0
DanG456 <80288370+DanG456@users.noreply.github.com>
2021-09-07 12:59:05 -0500

    Update presupuestos.models.js

--- models/presupuestos.models.js
+++ models/presupuestos.models.js
@@ -3,11 +3,11 @@ const {DataTypes} = require('sequelize');
 module.exports =(sequelize, type) =>{
     return sequelize.define('presupuesto',{
         idPresupuesto:{
-            type: type.INTEGER,
+            type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true
         },
-        proyecto: type.STRING,
-        version: type.STRING
+        proyecto: DataTypes.STRING,
+        version: DataTypes.STRING
     })
 }
